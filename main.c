66701b7fd6bcfabf73ffc9e7faba3211d4a7ea84
SathishKumar Loganathan <43195344+SathishKumar-Loganathan@users.noreply.github.com>
2022-01-18 22:45:35 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,40 +1,22 @@
 #include <stdio.h>
 #include <assert.h>
 #include "ColorCode_for_Telecommunications.h"
-#include "Verify_Color_Code_from_Number.h"
 
 const char* MajorColorNames[] = {
     "White", "Red", "Black", "Yellow", "Violet"
 };
+
 int numberOfMajorColors =
     sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
 
 const char* MinorColorNames[] = {
     "Blue", "Orange", "Green", "Brown", "Slate"
 };
+
 int numberOfMinorColors =
     sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
 
 const int MAX_COLORPAIR_NAME_CHARS = 16;
-
-int GetPairNumberFromColor(const ColorPair* colorPair) {
-    return colorPair->majorColor * numberOfMinorColors +
-            colorPair->minorColor + 1;
-}
-
-
-void testPairToNumber(
-    enum MajorColor major,
-    enum MinorColor minor,
-    int expectedPairNumber)
-{
-    ColorPair colorPair;
-    colorPair.majorColor = major;
-    colorPair.minorColor = minor;
-    int pairNumber = GetPairNumberFromColor(&colorPair);
-    printf("Got pair number %d\n", pairNumber);
-    assert(pairNumber == expectedPairNumber);
-}
 
 int main() {
     testNumberToPair(4, WHITE, BROWN);
