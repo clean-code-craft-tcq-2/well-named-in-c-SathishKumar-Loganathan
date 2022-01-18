bf68a70bba41d4f1302bb7616d801b8ffc893b83
SathishKumar Loganathan <43195344+SathishKumar-Loganathan@users.noreply.github.com>
2022-01-18 23:31:23 +0530

    Update Print_Wiring_Reference_Manual.c

--- Print_Wiring_Reference_Manual.c
+++ Print_Wiring_Reference_Manual.c
@@ -6,12 +6,11 @@
 void printWiringManual(void)
 {
 	ColorPair colorPair;
-	char colorPairNames[MAX_COLORPAIR_NAME_CHARS];
 	
 	printf("Pair No. \tMajor Color \t Minor Color");
 	for(int loopCount = 1; loopCount <= MAX_PAIR_NUMBER_LIMIT; loopCount++)
 	{
 		colorPair = GetColorFromPairNumber(loopCount);
-    		printf("%d \t%s \t%s\n", loopCount, MajorColorNames[colorPair.majorColor] , MinorColorNames[colorPair.minorColor]);
+    		printf("\n%d \t\t%s \t\t%s", loopCount, MajorColorNames[colorPair.majorColor] , MinorColorNames[colorPair.minorColor]);
 	}
 }
