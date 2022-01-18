e91661a82b5b5dba24a2be5aeea69a4669188cb1
SathishKumar Loganathan <43195344+SathishKumar-Loganathan@users.noreply.github.com>
2022-01-18 21:20:42 +0530

    Update Verify_Color_Code_from_Number.c

--- Verify_Color_Code_from_Number.c
+++ Verify_Color_Code_from_Number.c
@@ -18,9 +18,7 @@ void ColorPairToString(const ColorPair* colorPair, char* buffer)
             MinorColorNames[colorPair->minorColor]);
 }
 
-void testNumberToPair(int pairNumber,
-    enum MajorColor expectedMajor,
-    enum MinorColor expectedMinor)
+void testNumberToPair(int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor)
 {
     ColorPair colorPair = GetColorFromPairNumber(pairNumber);
     char colorPairNames[MAX_COLORPAIR_NAME_CHARS];
